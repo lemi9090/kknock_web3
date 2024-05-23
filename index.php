8991134ad39350880bede31cc453728e653b27ed
lemi9090 <lemi63069@gmail.com>
2024-05-23 23:34:55 +0900

    web_server

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>테스트입니다</title>
+        <style>
+        body{
+                background-color: blanchedalmond;
+        }
+        h1 {
+            color: chocolate;
+            font-size: x-large;
+        }
+        h2 {
+            font-weight: bold;
+            font-size: large;
+        }
+        ol {
+            color: darkgoldenrod;
+        }
+        </style>
+</head>
+
+<body>
+        <h1>
+            Welcome to Kknock Server
+        </h1>
+        <h2>
+            Team:KknockKnock
+        </h2>
+        <ol>
+            <li>Kong</li>
+            <li>Lee</li>
+            <li>Jung</li>
+            <li>Bong</li>
+        </ol>
+</body>
+
+</html>
