25e348c3fb17e4cf840d4dc553cfd76ed82f3264
lemi9090 <142860891+lemi9090@users.noreply.github.com>
2024-05-27 13:55:48 +0900

    Update login_sql.php

--- login_sql.php
+++ login_sql.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    $conn = mysqli_connect("localhost", "root", "Bcd1324!", "test_kknock");
+    $conn = mysqli_connect("localhost", "root", "??", "??");
     if ($conn === false) {
         error_log('MySQL 연결 실패: '.mysqli_connect_error());
         die("사이트에 문제가 발생했습니다. 관리자에게 문의하세요.");
