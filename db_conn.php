f0c0ad30d14a4ca6d5454a35c769b1d9739ff64a
lemi9090 <142860891+lemi9090@users.noreply.github.com>
2025-05-18 20:23:52 +0900

    Update db_conn.php
    change real env

--- db_conn.php
+++ db_conn.php
@@ -1,8 +1,8 @@
+
 <?php
 $conn = mysqli_connect("localhost", "root", "", "");
-    if ($conn === false) {
-        error_log('MySQL 연결 실패: '.mysqli_connect_error());
-        die("사이트에 문제가 발생했습니다. 관리자에게 문의하세요.");
-    }
+if ($conn === false){
+	error_log('mysql 연결실패: '.mysqli_connect_error());
+	die("사이트에 문제가 발생했습니다. 관리자에게 문의하세요.");
+	}
 ?>
-
