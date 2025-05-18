eb16fc3c0734dd5b35157f82cef34dabd9a8546e
lemi9090 <142860891+lemi9090@users.noreply.github.com>
2025-05-18 22:19:27 +0900

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,6 +1,6 @@
 
 <?php
-$conn = mysqli_connect("localhost", "root", "", "");
+$conn = mysqli_connect("localhost", "root", "", "tung");
 if ($conn === false){
 	error_log('mysql 연결실패: '.mysqli_connect_error());
 	die("사이트에 문제가 발생했습니다. 관리자에게 문의하세요.");
